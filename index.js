74a6a7fa42316a1931867ca3fe03cfb0ec5597a3
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-12-11 21:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number){
+  if(number>42){
+    return number-42;
+  }else {
+    return 42-number;
+  }
+}
+
+function distanceFromHqInFeet(number){
+        var num = distanceFromHqInBlocks(number);
+        return num*264;
+   }
+
+function distanceTravelledInFeet(arg1,arg2){
+    if(arg1>arg2){
+        return ((arg1-arg2)*264);
+    }
+     else{
+          return ((arg2-arg1)*264);
+    }
+}
+
+function calculatesFarePrice(start, destination)
+{
+
+}
